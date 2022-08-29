64427025c0c57bec30a010ebc679a579bfee0433
Ryan <ryan@mysocialhustle.com>
2022-08-29 07:58:30 -0400

    createCursor utility function

---
+++ src/utils/createCursor.ts
@@ -0,0 +1,28 @@
+import { Cursor } from '../generated/graphql'
+import { encode, decode } from './base64'
+import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client'
+
+interface OutputWithCursor extends DocumentClient.QueryOutput {
+  Cursor?: Cursor
+}
+
+export default function createCursor(
+  queryResponse: DocumentClient.QueryOutput,
+  cursor = {} as Cursor,
+): OutputWithCursor {
+  const { prevToken, nextToken } = cursor
+  const next: [] | null = nextToken ? decode(nextToken) : null
+  const prev: [] | null = prevToken ? decode(prevToken) : null
+  const keys: [] = next || prev || []
+
+  const { LastEvaluatedKey } = queryResponse
+  const res = queryResponse
+  const Cursor = {
+    prevToken: keys.length > 0 ? encode(keys.slice(0, keys.length - 1)) : null,
+    nextToken: LastEvaluatedKey ? encode([...keys, LastEvaluatedKey]) : null,
+  }
+  return {
+    ...res,
+    Cursor,
+  }
+}
