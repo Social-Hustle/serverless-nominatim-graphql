ebf14d854c875076e1249f035d7556a0f2bdbe5b
Ryan <ryan@mysocialhustle.com>
2022-09-23 15:22:25 -0400

    Fix base64 errors

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,7 +1,15 @@
-export const encode = (val: any): string => {
-  return btoa(JSON.stringify(val))
+export const encode = (args: any) => {
+  // btoa(JSON.stringify(args))
+  const str = JSON.stringify(args)
+  const buff = new (Buffer.from as any)(str)
+  return buff.toString('base64')
 }
 
-export const decode = (str: string): any => {
-  return JSON.parse(atob(str))
+export const decode = (str: string) => {
+  //JSON.parse(atob(str))
+  //const buff = new Buffer.from(str, "base64")
+  const buff = new Buffer(str, 'base64')
+  const newStr = buff.toString('ascii')
+  const json = JSON.parse(newStr)
+  return json
 }
